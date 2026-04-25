00c9cb463cc802b311c75dbd54c191480e8f97e3
Will McIntosh <will@wmm.co.nz>
2026-04-26 09:44:27 +1200

    Clean malformed frac commands in web math

--- OPERATIONS/scripts/generate_web_html.py
+++ OPERATIONS/scripts/generate_web_html.py
@@ -293,6 +293,9 @@ def latex_to_html(text: str) -> str:
     def transform_web_math(value: str) -> str:
         value = sanitize_yaml_text(value)
         value = value.replace("\\\\", "\\")
+        value = value.replace("\\f\\frac", "\\frac")
+        value = value.replace("\\fr ac", "\\frac")
+        value = value.replace("\\f rac", "\\frac")
         value = value.replace(r"\square", "□")
         return value
 
